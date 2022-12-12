17f4b1b1ca06ea2d48a20a7850291454edf702c1
Alexander <jayero@mail.ru>
2022-12-13 00:16:04 +0300

    solution_one

---
+++ task_1.py
@@ -0,0 +1,18 @@
+# 1. Вычислить число c заданной точностью d
+# in
+# Enter a real number: 9
+# Enter the required accuracy '0.0001': 0.000001
+
+# out
+# 9.000000
+
+
+from decimal import Decimal 
+
+def result():
+    number = Decimal(input('Enter a real number: ')) 
+    accuracy = number.quantize(Decimal(input('Enter the required accuracy: '))) 
+    return accuracy
+
+res = result()
+print(res)
