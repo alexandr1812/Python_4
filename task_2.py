d764a2565f0457ef7175f379e0d929a64fa4cbc4
Alexander <jayero@mail.ru>
2022-12-13 00:16:17 +0300

    solution_2

---
+++ task_2.py
@@ -0,0 +1,55 @@
+# 2. Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
+# Простые делители числа
+# Простые делители числа онлайн
+
+# in
+# 54
+
+# out
+# [2, 3, 3, 3]
+
+# in
+# 9990
+
+# out
+# [2, 3, 3, 3, 5, 37]
+
+# in
+# 650
+
+# out
+# [2, 5, 5, 13]
+
+
+# number = int(input()) 
+# s = [] 
+# a = 2 
+# while number > 1: 
+#     if number % a == 0: 
+#          s.append(a) 
+#          res = number//a 
+#          number = res  
+#     else: 
+#         number % a!= 0 
+#         a +=1 
+# print(s)
+
+def list_of_multipliers(number):
+    while number <= 0:
+        print('Enter a positive value')
+        number = int(input('Try again: '))
+
+
+    arr = []
+    divider = 2
+    while number > 1:
+        if number % divider == 0:
+            arr.append(divider)
+            res = number // divider
+            number = res
+        else:
+            number % divider != 0
+            divider += 1
+    return arr
+
+print(list_of_multipliers(int(input('Enter the number: '))))
