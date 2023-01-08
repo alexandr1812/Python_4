8778ec364f5f92cf00300829e4ff0b117d9b2037
Alexander1812 <jayero@mail.ru>
2023-01-08 06:13:50 +0300

    my_calc

--- main.py
+++ main.py
@@ -1,35 +1,10 @@
 import telebot
 from app import BOT_TOKEN
+from cal_keyboard import keyboard
 
 bot = telebot.TeleBot(BOT_TOKEN)
 value = ''
 old_value = ''
-
-keyboard = telebot.types.InlineKeyboardMarkup()
-keyboard.row(telebot.types.InlineKeyboardButton(' ', callback_data='no'),
-             telebot.types.InlineKeyboardButton('C', callback_data='C'),
-             telebot.types.InlineKeyboardButton('<=', callback_data='<='),
-             telebot.types.InlineKeyboardButton('/', callback_data='/'))
-
-keyboard.row(telebot.types.InlineKeyboardButton('7', callback_data='7'),
-             telebot.types.InlineKeyboardButton('8', callback_data='8'),
-             telebot.types.InlineKeyboardButton('9', callback_data='9'),
-             telebot.types.InlineKeyboardButton('*', callback_data='*'))
-
-keyboard.row(telebot.types.InlineKeyboardButton('4', callback_data='4'),
-             telebot.types.InlineKeyboardButton('5', callback_data='5'),
-             telebot.types.InlineKeyboardButton('6', callback_data='6'),
-             telebot.types.InlineKeyboardButton('-', callback_data='-'))
-
-keyboard.row(telebot.types.InlineKeyboardButton('1', callback_data='1'),
-             telebot.types.InlineKeyboardButton('2', callback_data='2'),
-             telebot.types.InlineKeyboardButton('3', callback_data='3'),
-             telebot.types.InlineKeyboardButton('+', callback_data='+'))
-
-keyboard.row(telebot.types.InlineKeyboardButton(' ', callback_data='no'),
-             telebot.types.InlineKeyboardButton('0', callback_data='0'),
-             telebot.types.InlineKeyboardButton(',', callback_data='.'),
-             telebot.types.InlineKeyboardButton('=', callback_data='='))
 
 
 @bot.message_handler(commands=['start', 'calculater'])
@@ -72,8 +47,8 @@ def callback_func(query):
                                   message_id=query.message.message_id, text=value, reply_markup=keyboard)
             old_value = value
     old_value = value
-    if value == "zero is not possible":
-        value = ''
+
+    if value == "zero is not possible": value = ''
 
 
 bot.polling(non_stop=False, interval=0)
